49eb25a51110a1a9cfe96f7f4e75a7dc5eb62e9d
Giggsa1703 <staroverovn155@gmail.com>
2026-05-22 13:42:10 +0300

    Add files via upload

--- fit_life.py
+++ fit_life.py
@@ -1,3 +1,4 @@
+#Поприветсвуем: узнаем имя, возраст
 user_name = input("Как тебя зовут? ")
 user_age = int(input("Сколько тебе лет? "))
 
@@ -5,13 +6,14 @@ user_age = int(input("Сколько тебе лет? "))
 user_weight = float(input("Введите ваш вес в кг: "))
 user_height = float(input("Введите ваш рост в метрах: "))
 
-# расчёт индекса массы тела
+#Индекс массы тела
 bmi = user_weight / (user_height ** 2)
 bmi_rounded = round(bmi, 1)
 
-#Расчет кол-ва воды
+#Расчёты количества воды
 water_ml = user_weight * 30
 water_l = water_ml / 1000
+
 
 print(f"Привет, {user_name}! Твой возраст {user_age}.")
 print(f"Твой индекс Массы Тела: {bmi_rounded}")
