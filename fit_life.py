7df0320691ff1b516c5c005dfff32abbb7b144a4
Giggsa1703 <staroverovn155@gmail.com>
2026-05-22 12:32:50 +0300

    Add files via upload

--- fit_life.py
+++ fit_life.py
@@ -1,26 +1,20 @@
-# Проект FitLife - MVP версия 1.0
+user_name = input("Как тебя зовут? ")
+user_age = int(input("Сколько тебе лет? "))
 
-
-# 1. Знакомство
-user_name = input("Как вас зовут? ")
-user_age = int(input("Сколько вам лет? "))
-
-# 2. Сбор данных
+#Рост, вес
 user_weight = float(input("Введите ваш вес в кг: "))
 user_height = float(input("Введите ваш рост в метрах: "))
 
-
 # расчёт индекса массы тела
 bmi = user_weight / (user_height ** 2)
-bmi = round(bmi, 1)
-
+bmi_rounded = round(bmi, 1)
 
 #Расчет кол-ва воды
 water_ml = user_weight * 30
-water_needed = water_ml / 1000
-
+water_l = water_ml / 1000
 
 print(f"Привет, {user_name}! Твой возраст {user_age}.")
-print(f"Твой индекс Массы Тела: {bmi}")
-print(f"Рекомендуемая норма воды: {water_needed:.1f} л. в день")
+print(f"Твой индекс Массы Тела: {bmi_rounded}")
+print(f"Рекомендуемая норма воды: {water_l:.1f} л. в день")
+
 print("Расчет окончен. Будьте здоровы!")
